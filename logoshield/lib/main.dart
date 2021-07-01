58e947c8398a7862f99029b406768ec4103d0bb5
LAPTOP-M0FKNJEP\LENOVO <amirhusein008@gmail.com>
2021-07-01 08:04:19 +0700

    no message

--- logoshield/lib/main.dart
+++ logoshield/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:logoshield/pages/homeView.dart';
-import 'package:logoshield/pages/scanPageO.dart';
 import 'package:logoshield/pages/splashScreen.dart';
 
 void main() {
